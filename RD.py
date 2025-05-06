2d30f0530603f413ebc0e3320008cfa6217b3ec0
666899734 <3675915377@qq.com>
2025-05-06 16:10:00 +0800

    Create RD.py

---
+++ RD.py
@@ -0,0 +1,4 @@
+def readme():
+   print("this is a project which can release OnePlus Kernel with Root")
+ readme()
+ yaap = 'repo init -u https://https://github.com/OnePlusOSS/kernel_manifest/tree/oneplus/sm8750 -b refs/heads/kplatform -m oneplus_13.xml --depth=1'
